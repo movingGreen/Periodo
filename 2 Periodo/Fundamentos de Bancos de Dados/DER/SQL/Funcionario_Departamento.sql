3fe1e531209c97371ad61ec5a450d4afed4dc3c1
Elizeu <el_vailant@hotmail.com>
2022-04-30 19:17:44 -0300

    30-04-2022
    Atividades até está data.

---
+++ 2 Periodo/Fundamentos de Bancos de Dados/DER/SQL/Funcionario_Departamento.sql
@@ -0,0 +1,24 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+
+
+CREATE TABLE FUNCIONARIO (
+ID_Funcionario INTEGER PRIMARY KEY,
+Nome VARCHAR(50),
+Nu_Matricula INTEGER,
+Sexo CHAR(1),
+Email VARCHAR(10),
+DT_Admissao DATE,
+Qt_Projetos INTEGER,
+Telefone VARCHAR(13),
+FOREIGN KEY(ID_Departamento) REFERENCES DEPARTAMENTO (ID_Departamento)
+)
+
+CREATE TABLE DEPARTAMENTO (
+ID_Departamento INTEGER PRIMARY KEY,
+Nome VARCHAR(50),
+Sigla CHAR(8),
+Responsavel VARCHAR(50),
+Ramal VARCHAR(13)
+)
